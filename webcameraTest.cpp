55e6349e01412ce49af38c911eb53ca457d1b017
hernandezurbina <hernandezurbina@gmail.com>
2020-03-05 22:28:47 +0000

    First commit

---
+++ webcameraTest.cpp
@@ -0,0 +1,27 @@
+/* 
+/usr/local/include/opencv4/opencv2
+g++ webcameraTest.cpp -I/usr/local/include/opencv4 -L/usr/local/lib -lopencv_core -opencv_imgproc -lopencv_highgui -lopencv_videoio -o main.out
+*/
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+ 
+int main() {
+	VideoCapture stream1(0);   //0 is the id of video device.0 if you have only one camera.
+ 
+	if (!stream1.isOpened()) { //check if video device has been initialised
+		cout << "cannot open camera";
+	}
+ 
+	//unconditional loop
+	while (true) {
+		Mat cameraFrame;
+		stream1.read(cameraFrame);
+		imshow("cam", cameraFrame);
+		if (waitKey(30) >= 0)
+		break;
+	}
+	return 0;
+}
